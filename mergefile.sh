de8b3e235041433fa8101a195323cda92de3dd9c
Fbigun Deo <olutyo@gmail.com>
2014-10-10 13:25:42 +0800

    Initial commit

---
+++ mergefile.sh
@@ -0,0 +1,17 @@
+﻿#!/bin/bash
+
+# rols mergefile a b to c
+
+file_a=(`cat $1`)
+file_b=(`cat $2`)
+
+length=${#file_a[@]}
+lengthn=${#filea[0]}
+num=`expr length / lengthn`
+
+
+for(index=0;index<num;index++);
+do
+echo "{file_a[index]} ${file_b[index]}" >> $3
+done
+
